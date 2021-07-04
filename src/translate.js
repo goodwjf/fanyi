a3c8e61c97632f6c34e18d95a4d0599c513a217a
goodwjf <wangjianfenga1@163.com>
2021-07-04 11:22:30 +0800

    切换翻译账号

--- src/translate.js
+++ src/translate.js
@@ -18,8 +18,8 @@ function setLang(opt) {
 
 function translate(q, fn) {
   const salt = Math.random();
-  const appid = '20210320000735744';
-  const appSecret = 'r8zOD7lNfOTkcByxpbPC';
+  const appid = '20210629000875206';
+  const appSecret = 'WxPbDm4oAt3yzely16oP';
   const sign = md5(appid + q + salt + appSecret);
   const from = option.from;  
   const to = option.to;
@@ -77,3 +77,44 @@ module.exports = {
   translate,
   setLang
 }
+
+
+
+// const obj = {
+//   message: {
+//     '现场百家乐': '现场百家乐',
+//     '保险百家乐': '保险百家乐',
+//     '龙宝百家乐': '龙宝百家乐',
+//     activity: {
+//         nav: {
+//           all: '全部',
+//           vip: 'VIP红利',
+//           newbie: '新手红利',
+//           topic: '专题红利',
+//           regular: '常规红利',
+//           timeLimited: '限时红利'
+//         },
+//         tag: {
+//           all: '全部',
+//           vip: 'VIP',
+//           newbie: '新手',
+//           topic: '专题',
+//           regular: '常规',
+//           timeLimited: '限时'
+//         }
+//     },
+//     immediateRegistration: '立即注册',
+//     totalMoney: '总余额',
+//     accountBalance: '账户余额',
+//     selfWashingCount: '可洗码金额',
+//     historyTitle: '历史记录', 
+//   }
+// }
+// let lang = {}
+// translate(JSON.stringify(obj.message), (res) => {
+
+//   // conversion
+//   // conversion
+//   console.log(res[0].dst)
+   
+// })
